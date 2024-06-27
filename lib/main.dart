82c1c5afc8db7cef65ec7436c7049943495dbcb8
alex <mso7nueh@mail.ru>
2024-06-27 17:03:52 +0300

    map

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:pusher_client/pusher_client.dart';
 
+Map<String, String> params = {
+  'host': 'socket.mobile-stage.eatvio.ru',
+  'authEndpoint': 'https://mobile-stage.eatvio.ru/broadcasting/auth',
+  'key': 'EiquuV7uy3aiReith0te',
+  'channelName': 'private-new-chat-messages.21',
+  'token': 'Bearer 63|laravel_sanctum_D72Pibpmw5A4zb9WvQUF12HqBWg3daWKDLq7eIoPbb09bb74',
+};
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   PusherOptions options = PusherOptions(
-    host: 'socket.mobile-stage.eatvio.ru',
+    host: params['host']!,
     wssPort: 443,
     encrypted: true,
     auth: PusherAuth(
-      'https://mobile-stage.eatvio.ru/broadcasting/auth',
+      params['authEndpoint']!,
       headers: {
-        'Authorization': 'Bearer 63|laravel_sanctum_D72Pibpmw5A4zb9WvQUF12HqBWg3daWKDLq7eIoPbb09bb74',
+        'Authorization': params['token']!,
       },
     ),
   );
 
   PusherClient pusherClient = PusherClient(
-    'EiquuV7uy3aiReith0te',
+    params['key']!,
     options,
     autoConnect: true,
     enableLogging: true,
@@ -27,7 +35,7 @@ void main() async {
 
   pusherClient.registerListener('classId', (data) => print(data));
 
-  pusherClient.subscribe('private-new-chat-messages.21');
+  pusherClient.subscribe(params['channelName']!);
 
   runApp(const MyApp());
 }
@@ -40,4 +48,3 @@ class MyApp extends StatelessWidget {
     return const Placeholder();
   }
 }
-
